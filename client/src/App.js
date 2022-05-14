1bb851acd22c2c9c52d0223a5ee64439e3606134
wbruns <wbruns07@gmail.com>
2022-05-13 21:19:41 -0500

    having issues with profile page/component. user variable is created with username but doesn't have a populated thought array or anything to do with them

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-
 import Home from './pages/Home';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -18,15 +22,45 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className='flex-column justify-flex-start min-100-vh'>
+      <Router>
+        <div className='flex-column justify-flex-start min-100-vh'>
           <Header />
           <div className='container'>
-            <Home />
+            <Routes>
+              <Route
+                path="/"
+                element={<Home />}
+              />
+              <Route
+                path="/login"
+                element={<Login />}
+              />
+              <Route
+                path="/signup"
+                element={<Signup />}
+              />
+              <Route
+                path="/profile"
+              >
+                <Route
+                  path=":username"
+                  element={<Profile />}
+                />
+              </Route>
+              <Route
+                path="/thought/:id"
+                element={<SingleThought />}
+              />
+              <Route
+                path="*"
+                element={<NoMatch />}
+              />
+            </Routes>
           </div>
           <Footer />
-      </div>
+        </div>
+      </Router>
     </ApolloProvider>
-    
   );
 }
 
